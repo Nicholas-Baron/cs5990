3c67e34076ae6803d6a8e02e8b6482aced355a5b
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-03-14 21:23:15 -0700

    Improve slurm settings

--- watts-strogatz.sh
+++ watts-strogatz.sh
@@ -3,6 +3,8 @@
 #SBATCH --output=watts_out.txt
 #SBATCH --ntasks=1
 #SBATCH --time=1:00:00
+#SBATCH --partition=compute
 #SBATCH --mem-per-cpu=500M
+#SBATCH -c 16
 
 srun python ./watts-strogatz.py < input.txt
