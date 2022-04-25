8b611b8e5c8555f8a094b6e3e9f265d1bb122b56
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-04-25 15:10:00 -0700

    Fully implement Facebook script

--- hw2/facebook_serial.py
+++ hw2/facebook_serial.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
-from backend import load_graph
+from backend import load_graph, serial_betweenness_centrality, print_timing
 import networkx as nx
+from pprint import pprint
 
 # Facebook dataset
 # no parallelism
 
 g = load_graph("facebook_combined.txt.gz")
+print_timing("Loading Graph")
+
 print(g)
+
+centralities = serial_betweenness_centrality(g)
+print_timing("Betweenness Centrality")
+
+pprint(centralities)
