d524b46cfaf3da1093e76c8314c81d02fe748a3e
clcannon <71797046+clcannon@users.noreply.github.com>
2022-04-30 12:43:08 -0700

    Update twitter_parallel script w function call

--- hw2/twitter_parallel.py
+++ hw2/twitter_parallel.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 
+from backend import (
+    load_graph,
+    parallel_betweenness_centrality,
+    print_centrality_data,
+    print_timing,
+)
+
+import networkx as nx
+
 # Twitter dataset
 # with parallelism
+
+g = load_graph("twitter_combined.txt.gz")
+print_timing("Loading Graph")
+
+print(g)
+
+centralities = parallel_betweenness_centrality(g)
+print_timing("Betweenness Centrality")
+
+print_centrality_data("twitter_parallel_raw_output.txt", centralities)
