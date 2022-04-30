bb3f0b747af2e00c0dd531065b2bf5c8bca67562
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-04-30 11:40:01 -0700

    Make extracted functions sensible

--- hw2/mpi_floyd.py
+++ hw2/mpi_floyd.py
@@ -6,28 +6,6 @@ from mpi4py import MPI
 from pprint import pprint
 from typing import Dict, Tuple, Set
 
-def compute_distance():
-    i = rank * num_nodes_per_proc + off
-    for j in range(NODE_COUNT):
-        if k == j or j == i:
-            continue
-    
-        possible_new_path = dist[i][k] + dist[k][j]
-        if dist[i][j] > possible_new_path:
-            dist[i][j] = possible_new_path
-    return i, j
-
-def trasmit_data():
-    for proc in range(num_proc):
-        # Row that is transmitted
-        index_to_transmit = proc * num_nodes_per_proc + off
-    
-        sent_row = comm.bcast(dist[index_to_transmit], root=proc)
-        # apply the new row
-        dist[index_to_transmit] = sent_row
-        for (i, val) in enumerate(sent_row):
-            dist[i][index_to_transmit] = val
-    return proc
 
 comm = MPI.COMM_WORLD
 num_proc = comm.Get_size()
@@ -82,17 +60,42 @@ if rank == 0:
 num_nodes_per_proc = NODE_COUNT // num_proc
 remainder_nodes = NODE_COUNT % num_proc
 
+
+def compute_distance(k: int, off: int) -> int:
+    i = rank * num_nodes_per_proc + off
+    for j in range(NODE_COUNT):
+        if k == j or j == i:
+            continue
+
+        possible_new_path = dist[i][k] + dist[k][j]
+        if dist[i][j] > possible_new_path:
+            dist[i][j] = possible_new_path
+    return i
+
+
+def trasmit_data(i: int, off: int):
+    for proc in range(num_proc):
+        # Row that is transmitted
+        index_to_transmit = proc * num_nodes_per_proc + off
+
+        sent_row = comm.bcast(dist[index_to_transmit], root=proc)
+        # apply the new row
+        dist[index_to_transmit] = sent_row
+        for (i, val) in enumerate(sent_row):
+            dist[i][index_to_transmit] = val
+
+
 # For each intermediate node k,
 for k in range(NODE_COUNT):
     for off in range(num_nodes_per_proc):
         # Offset i based on processor amount
-        i, j = compute_distance()
-        proc = trasmit_data()
+        i = compute_distance(k, off)
+        trasmit_data(i, off)
 
     # process remainders
     if rank == num_proc - 1:
         for off in range(remainder_nodes):
-            i, j = compute_distance()
+            i = compute_distance(k, off)
 
     # transmit remainders
     for off in range(remainder_nodes):
