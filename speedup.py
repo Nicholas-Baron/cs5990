bd80f275fab72ff16493f04e6e8ba59f558e5af8
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-05-08 18:13:19 -0700

    The speedup script now outputs all images needed in 1 run

--- speedup.py
+++ speedup.py
@@ -26,14 +26,19 @@ def calc_speedup(times: Mapping[int, Union[int, float]]) -> Dict[int, float]:
 
 
 def plot(filename: str, x_axis: str, data: Mapping[int, Union[int, float]]):
-    pyplot.plot(
-        data.keys(),
-        data.values(),
-    )
+    # Clear the previous run
+    pyplot.clf()
+    pyplot.cla()
+
+    # Plot the input data
+    pyplot.plot(data.keys(), data.values())
     pyplot.title(filename)
     pyplot.ylabel(x_axis)
     pyplot.xlabel("Number of MPI processes")
-    pyplot.savefig(filename)
+    pyplot.savefig(filename, bbox_inches="tight")
 
 
+plot("Facebook Time", "Time in seconds", facebook)
 plot("Twitter Time", "Time in seconds", twitter)
+plot("Facebook Speedup", "Speedup relative to serial", calc_speedup(facebook))
+plot("Twitter Speedup", "Speedup relative to serial", calc_speedup(twitter))
